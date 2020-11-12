e9766d767fe1f8018c59874f57f2012b63ee7204
iamjaneodey <janeodeys98@gmail.com>
2020-11-12 04:44:13 -0800

    made additions to the script file

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
-window.addEventListener("keydown", function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
     if(!audio) return; // stops the function from running
     audio.currentTime = 0 // rewinds to the start 
     audio.play();
-    console.log(key)
+    key.classList.add("playing")
+}
+
+function removeTransition(e){
+    if(e.propertName !== "transform") return // skip it if it is not a transform
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key=>{
+    key.addEventListener("transitionend", removeTransition)
 })
+
+window.addEventListener("keydown", playSound)
